bb5f9a7d7da6933de69a729ba2511e749982121e
Doniyorbek <programmmerqety1@gmail.com>
2024-04-30 16:44:37 +0500

    authetications

--- test_app/views.py
+++ test_app/views.py
@@ -2,12 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.request import Request
-from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import BasicAuthentication
 
 
 from .models import Test,User
 from .serializers import TestSerializer, UserSerializer, ResultSerializer
+
 
 class UserRegistrationView(APIView):
     # register user
@@ -32,7 +33,9 @@ class UserRegistrationView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
     
 class TestView(APIView):
-    permission_classes  = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [BasicAuthentication]
+    # permission_classes  = [IsAuthenticated]
     def get(self, request: Request, pk=None,q_type=None,q_subject=None) -> Response:
         if pk is None and q_subject is None and q_type is None:
             tests = Test.objects.all()
@@ -60,28 +63,12 @@ class TestView(APIView):
             except Test.DoesNotExist:
                 return Response(status=status.HTTP_404_NOT_FOUND)
     
-    # post method 
-    def post(self, request:Request):
-        data = request.data
-        serializer = TestSerializer(data=data)
-        question = data["question_str"]
-        print(request.user)
-        find = Test.objects.filter(question_str = question )
-        print(len(find))
-        
-        if serializer.is_valid():
-            if find==0 and request.user == 'admin':
-                serializer.save()
-                return Response("Congrulations this test add baza!!!",status=status.HTTP_201_CREATED)
-            elif find != 0 and  request.user =='admin':
-                return Response("this test already exist", status=status.HTTP_208_ALREADY_REPORTED)
-            elif request.user !='admin':
-                return Response("You cannot create a test!!!", status=status.HTTP_401_UNAUTHORIZED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 # result class 
 class ResultView(APIView):
-    pass
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [BasicAuthentication]
+    # get method
+    
 
 
