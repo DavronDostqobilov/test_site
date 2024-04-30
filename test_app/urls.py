41683622a557e6e63acf8e3f483940986d3f4b25
Doniyorbek <programmmerqety1@gmail.com>
2024-04-30 11:00:54 +0500

    add get users

--- test_app/urls.py
+++ test_app/urls.py
@@ -4,6 +4,7 @@ from .views import TestView,UserRegistrationView
 
 urlpatterns = [
     path('register/' , UserRegistrationView.as_view()),
+    path('register/<int:pk>/', UserRegistrationView.as_view()),
     path('test/', TestView.as_view()),
     path('test/<int:pk>/', TestView.as_view()),
     path('test/<str:q_type>/', TestView.as_view()),
