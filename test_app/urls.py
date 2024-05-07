405e9b63c16f5a3cf21bf26765f2722947fa8d5d
Doniyorbek <programmmerqety1@gmail.com>
2024-05-07 17:25:12 +0500

    create loginview

--- test_app/urls.py
+++ test_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TestView,UserRegistrationView, ResultView
+from .views import TestView,UserRegistrationView, ResultView, LoginView
 
 
 urlpatterns = [
     path('register/' , UserRegistrationView.as_view()),
+    path('auth/', LoginView.as_view()),
     path('test/', TestView.as_view()),
     path('test/<int:pk>/', TestView.as_view()),
     path('test/<str:q_type>/', TestView.as_view()),
